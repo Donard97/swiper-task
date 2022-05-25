26cb9a19b2bcc689da47d2629b9180f2c910f9ad
Donard97 <donardgolaj@gmail.com>
2022-05-25 22:19:58 +0200

    Fetch api

--- src/components/SwiperContainer.js
+++ src/components/SwiperContainer.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const SwiperContainer = () => (
-  <>
-    <div className="swiper">
-      <h2>MASTER WIZR Modules</h2>
-    </div>
-  </>
-);
+const SwiperContainer = () => {
+  const [data, setData] = useState();
+
+  fetch('https://jsonplaceholder.typicode.com/photos')
+    .then((response) => response.json())
+    .then((responseData) => setData(responseData));
+
+  return (
+    <>
+      <div className="swiper">
+        <h2>MASTER WIZR Modules</h2>
+        <div className="icons-navigation">
+          <img src="#" alt="test" />
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default SwiperContainer;
